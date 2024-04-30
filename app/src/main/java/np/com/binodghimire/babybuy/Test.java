328c8012e167f018d15025e97946a54afa04cf0b
iambinod2000 <ghimirebinod849@gmail.com>
2024-04-30 14:51:03 +0545

    application uploaded to github

---
+++ app/src/main/java/np/com/binodghimire/babybuy/Test.java
@@ -0,0 +1,20 @@
+package np.com.binodghimire.babybuy;
+
+/**
+ * Created  on 10/11/2022.
+ */
+class Test {
+
+}
+
+/*
+Recycler View
+- ViewGroup (Widget)
+- Adapter
+- ViewHolder
+- LayoutManager
+
+
+
+
+ */
